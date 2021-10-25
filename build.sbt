9898ec2fd2de640817830e2bef2e0ef29a237847
MrMoix <gaetan.moix@students.hevs.ch>
2021-10-25 18:38:07 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "50BestRestaurantProjet"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/Restaurant.scala
@@ -0,0 +1,16 @@
+import com.github.tototoshi.csv.CSVReader
+
+import java.io.File
+
+object Restaurant {
+  def main(args: Array[String]): Unit = {
+    case class Restaurant(Ranking: String, Restaurant: String, City: String, Country: String, Lat: Float, Lon: Float, Stars: String, Chef: String, Website: String, Menu: Int, Currency: String, Description: String)
+
+
+    val reader = CSVReader.open(new File("src/main/02-50BestRestaurants.csv"))
+    val t = reader.toStream
+
+
+    print(t.toList)
+  }
+}
