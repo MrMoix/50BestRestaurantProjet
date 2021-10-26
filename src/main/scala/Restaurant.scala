342da3feaee4e2389cc973797d594c6dca982c39
MrMoix <gaetan.moix@students.hevs.ch>
2021-10-26 13:37:53 +0200

    Lecture DB + Stockage Variables

--- src/main/scala/Restaurant.scala
+++ src/main/scala/Restaurant.scala
@@ -4,13 +4,15 @@ import java.io.File
 
 object Restaurant {
   def main(args: Array[String]): Unit = {
-    case class Restaurant(Ranking: String, Restaurant: String, City: String, Country: String, Lat: Float, Lon: Float, Stars: String, Chef: String, Website: String, Menu: Int, Currency: String, Description: String)
+    case class Restaurant(Ranking: Int, Restaurant: String, City: String, Country: String, Lat: Double, Lon: Double, Stars: String, Chef: String, Website: String, Menu: Double, Currency: String, Description: String)
 
 
     val reader = CSVReader.open(new File("src/main/02-50BestRestaurants.csv"))
-    val dataset = reader.toStream
+    val dataset = reader.toStream.toList.drop(1)
 
+  val restaurants = dataset.map(resto => Restaurant(resto(0).toInt,resto(1).trim,resto(2), resto(3),resto(4).toDouble,resto(5).toDouble, resto(6), resto(7), resto(8), resto(9).toDouble,resto(10), resto(11)))
 
-    dataset.drop(1).foreach(t => for (test <- t) println(test.trim))
+    //insertColumn.foreach(println)
+println(restaurants)
   }
 }
