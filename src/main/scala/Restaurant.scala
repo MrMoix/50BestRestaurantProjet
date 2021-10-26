2f44b34ef59e64a3aa5d7b859e57ba2caefee547
MrMoix <gaetan.moix@students.hevs.ch>
2021-10-26 14:47:39 +0200

    Top 10 resquest

--- src/main/scala/Restaurant.scala
+++ src/main/scala/Restaurant.scala
@@ -10,9 +10,11 @@ object Restaurant {
     val reader = CSVReader.open(new File("src/main/02-50BestRestaurants.csv"))
     val dataset = reader.toStream.toList.drop(1)
 
-  val restaurants = dataset.map(resto => Restaurant(resto(0).toInt,resto(1).trim,resto(2), resto(3),resto(4).toDouble,resto(5).toDouble, resto(6), resto(7), resto(8), resto(9).toDouble,resto(10), resto(11)))
+    val restaurants = dataset.map(resto => Restaurant(resto(0).toInt, resto(1).trim, resto(2), resto(3), resto(4).toDouble, resto(5).toDouble, resto(6), resto(7), resto(8), resto(9).toDouble, resto(10), resto(11)))
 
     //insertColumn.foreach(println)
-println(restaurants)
+    //println(restaurants)
+
+    restaurants.filter(x=>restaurants(x.Ranking-1).Ranking.toInt<=10).foreach(println)
   }
 }
