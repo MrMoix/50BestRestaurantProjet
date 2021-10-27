42902b0477cbe03664cfbe3244eebf58571e332c
MrMoix <gaetan.moix@students.hevs.ch>
2021-10-27 12:44:56 +0200

    Trying implement Traits

--- src/main/scala/Restaurant.scala
+++ src/main/scala/Restaurant.scala
@@ -4,10 +4,19 @@ import java.io.File
 
 object Restaurant extends App {
 
-  case class Restaurant(Ranking: Int, Restaurant: String, City: String, Country: String, Lat: Double, Lon: Double, Stars: String, Chef: String, Website: String, Menu:Double, Currency: String, Description: String) {
-    def intial(Menu: Double) = Menu
+  trait Activity {
+    val name: String
+    val city: String
+    val country: String
+    val lat: Double
+    val lon: Double
 
-    def increase(Menu: Double) = (Menu + Menu * 0.5).toDouble
+  }
+
+  case class Restaurant(ranking: Int, name: String, city: String, country: String, lat: Double, lon: Double, stars: String, chef: String, website: String, menu: Double, currency: String, description: String) extends Activity {
+    def intial(menu: Double) = menu
+
+    def increase(menu: Double) = (menu + menu * 0.5).toDouble
   }
 
 
@@ -16,9 +25,9 @@ object Restaurant extends App {
 
   val restaurants = dataset.map(resto => Restaurant(resto(0).toInt, resto(1).trim, resto(2), resto(3), resto(4).toDouble, resto(5).toDouble, resto(6), resto(7), resto(8), resto(9).toDouble, resto(10), resto(11)))
 
-  //val resto1Price = restaurants(0).intial(restaurants(0).Menu)
-  println(restaurants(0).increase(restaurants(0).Menu))
-  
+  val resto1Price = restaurants(0).intial(restaurants(0).menu)
+  //println(resto1Price)
+  //println(restaurants(0).increase(restaurants(0).menu))
 
 
   //println("Prix initial: " + resto1Price)
@@ -27,23 +36,23 @@ object Restaurant extends App {
   //insertColumn.foreach(println)
   //println(restaurants)
 
-  val test = restaurants.filter(x => restaurants(x.Ranking - 1).Ranking.toInt <= 10)
+  val test = restaurants.filter(x => restaurants(x.ranking - 1).ranking.toInt <= 10)
   //test.foreach(println)
 
-  val test2 = restaurants.groupBy(x => x.Country.contains("France")).map(x => x)
+  val test2 = restaurants.groupBy(x => x.country.contains("France")).map(x => x._2)
   //test2.foreach(println)
 
-  val test3 = restaurants.sortBy(_.Restaurant).groupMap(x => x.Menu)(x => x.Restaurant)
-  // test3.foreach(println)
+  val test3 = restaurants.sortBy(_.name).map(_.name)
+  test3.foreach(println)
 
-  val test4 = restaurants.filter(x => x.Currency.contains("EUR")).map(x => x.Restaurant)
+  val test4 = restaurants.filter(x => x.currency.contains("EUR")).map(x => x.name)
 
   //test4.foreach(println)
 
-  val test5 = restaurants.filter(x => x.Stars.contains("3")).map(x => x.Menu).sum / restaurants.length
+  val test5 = restaurants.filter(x => x.stars.contains("3")).filter(x => x.currency.contains("EUR")).map(x => x.menu).sum / restaurants.filter(x => x.stars.contains("3")).filter(x => x.currency.contains("EUR")).length
 
 
-  //print(test5)
+  print("Prix moyen du menu des restaurants 3 étoiles en EUR : "+test5)
 
 
 }
